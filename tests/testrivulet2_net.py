f07f166c7b86080aa8749495535fdea847c17524
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2022-06-30 13:07:46 +0200

    Improved segmentation.py

--- tests/testrivulet2_net.py
+++ tests/testrivulet2_net.py
@@ -6,12 +6,12 @@ if __name__ == '__main__':
     start_time = time.time()
 
     # filename = r'H:\Duet\Visualizations\MicroscopeImages\HyperStack.tif'
-    filename = r"C:\Users\twh\Desktop\HyperStack_small.tif"
+    filename = r"C:\Users\twh\Desktop\HyperStack.tif"
 
     tracer = Tracer()
     tracer.set_file(filename)
     tracer.asynchronous_off()
-    tracer.set_tolerance(0.25)
+    tracer.set_tolerance(0.10)
     neurons = tracer.execute()
 
     print(f'\n\nDone in {time.time() - start_time} s')
